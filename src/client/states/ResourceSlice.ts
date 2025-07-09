84bbdade42cea0b2464e33b146a2259eacf54053
Nicholas Osto <nicholas.osto@gmail.com>
2025-07-09 06:49:17 -0400

    feat: compute and sync resources

--- src/client/states/ResourceSlice.ts
+++ src/client/states/ResourceSlice.ts
@@ -19,10 +19,11 @@ import {
 	RESOURCE_KEYS,
 	DEFAULT_RESOURCES,
 	ResourceDTO,
-	createResourceState,
+	createStateResources,
 	ResourceStateMap,
 	ResourceDataMap,
 } from "shared/definitions/Resources";
+import { Value } from "@rbxts/fusion";
 
 export default class ResourceSlice {
 	private static instance: ResourceSlice;
@@ -32,33 +33,43 @@ export default class ResourceSlice {
 	public Stamina: ResourceStateMap["Stamina"];
 
 	private constructor() {
-		this.Health = createResourceState(DEFAULT_RESOURCES.Health);
-		this.Mana = createResourceState(DEFAULT_RESOURCES.Mana);
-		this.Stamina = createResourceState(DEFAULT_RESOURCES.Stamina);
+		const states = createStateResources(DEFAULT_RESOURCES);
+		this.Health = states.Health;
+		this.Mana = states.Mana;
+		this.Stamina = states.Stamina;
 	}
 
 	public UpdateResources(data: ResourceDataMap): void {
 		print("Updating resources with data:", data);
-		this.Health = createResourceState(data.Health);
-		this.Mana = createResourceState(data.Mana);
-		this.Stamina = createResourceState(data.Stamina);
+		(RESOURCE_KEYS as readonly ResourceKey[]).forEach((key) => {
+			this.UpdateResource(key, data[key]);
+		});
 	}
 
 	public UpdateResource(key: ResourceKey, data: ResourceDTO): void {
 		print(`Updating resource ${key} with data:`, data);
 		switch (key) {
 			case "Health":
-				this.Health = createResourceState(data);
+				this._assign(this.Health, data);
 				break;
 			case "Mana":
-				this.Mana = createResourceState(data);
+				this._assign(this.Mana, data);
 				break;
 			case "Stamina":
-				this.Stamina = createResourceState(data);
+				this._assign(this.Stamina, data);
 				break;
-			default:
-				print(`Unknown resource key: ${key}`);
-				break;
+		}
+	}
+
+	private _assign(state: ResourceStateMap[ResourceKey], data: ResourceDTO) {
+		state.current.set(data.current);
+		state.max.set(data.max);
+		if (data.regenPerSecond !== undefined) {
+			if (state.regenPerSecond) {
+				state.regenPerSecond.set(data.regenPerSecond);
+			} else {
+				state.regenPerSecond = Value(data.regenPerSecond);
+			}
 		}
 	}
 
